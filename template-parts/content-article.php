c52e93da40a351545ae7deb9f075713610dc336e
Andrei Racasan <aracasan@outlook.com>
2021-03-01 21:28:09 +0000

    create archive, comms and index

--- template-parts/content-article.php
+++ template-parts/content-article.php
@@ -1,2 +1,20 @@
-<?php
-the_content();
+<div class="container">
+  <header class="content-header">
+    <div class="meta mb-3">
+      <span class="date">Published on <?php the_date(); ?></span>
+      <span class="tag">
+        <?php the_tags('<span class="tag">', '</span> <span class="tag">', '</span>'); ?>
+      </span>
+      <span class="comment"><a href="#comments"><?php comments_number(); ?></a></span>
+    </div>
+  </header>
+
+  <?php
+  the_content();
+  ?>
+
+  <?php
+  comments_template();
+  ?>
+
+</div>
